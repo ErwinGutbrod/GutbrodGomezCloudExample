0e821463e08ec7e1b118ef5fdf85968d8e0e80f4
Erwin <Erwin@DESKTOP-0D3IOUK>
2016-10-18 01:02:50 -0300

    DocInformation

---
+++ GutbrodGomezCloudExample/src/Utils/DocumentInformation.java
@@ -0,0 +1,14 @@
+package Utils;
+
+public class DocumentInformation {
+	
+	String title;
+	
+	public void setTitle(String title){
+		this.title = title;
+	}
+	
+	public String getTitle(){
+		return this.title;
+	}
+}
